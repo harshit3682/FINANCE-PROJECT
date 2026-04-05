95548448b1a1b2e1358cb4f47c285d25d9c12399
harshit3682 <harshitrautela3682@gmail.com>
2026-04-06 01:17:29 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Replace REPO_NAME with your GitHub repository name before deploying
-  base: '/REPO_NAME/',
+  base: 'FINANCE-PROJECT',
 })
 
